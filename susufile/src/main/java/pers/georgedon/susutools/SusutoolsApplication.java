98840f74fd64c9ed10202ba6447de97bcddedb7d
GeorgeDon <826481939@qq.com>
2019-12-25 00:04:11 +0800

    工程初始化

---
+++ susufile/src/main/java/pers/georgedon/susutools/SusutoolsApplication.java
@@ -0,0 +1,13 @@
+package pers.georgedon.susutools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusutoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusutoolsApplication.class, args);
+	}
+
+}
